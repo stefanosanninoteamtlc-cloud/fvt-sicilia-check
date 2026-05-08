485978a5da5fb082eec162a3963b76bfb5897656
stefanosanninoteamtlc-cloud <stefanosannino.teamtlc@gmail.com>
2026-05-08 16:23:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,18 @@ logo = Image.open("logonuovo.png")
 st.set_page_config(page_title="Permitting FVT Sicilia 2026", layout="wide")
 
 st.image(logo, width=100)
-st.title("⚖️ Expert System: Iter Autorizzativo FVT Sicilia 2026")
+st.markdown("""
+        <style>
+            .title-text {
+                display: flex;
+                align-items: center;
+                height: 80px; /* Deve essere simile alla width/altezza del logo */
+                margin: 0;
+            }
+        </style>
+        <h1 class="title-text">Expert System: Iter Autorizzativo FVT Sicilia 2026</h1>
+    """, unsafe_allow_html=True)
+st.title("Expert System: Iter Autorizzativo FVT Sicilia 2026")
 
 # Creiamo due colonne: la prima occupa il 70% dello spazio, la seconda il 30%
 col_principale, col_info = st.columns([0.7, 0.3])
